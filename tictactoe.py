26b1ba8b4b4c926f687ab4e5eb3f8218aadb3c0b
salt-die <naclmolecule@gmail.com>
2019-09-25 23:53:05 -0500

    condensed f assignment to one-line

--- tictactoe.py
+++ tictactoe.py
@@ -29,9 +29,7 @@ We operate under a few constraints:
     and vertical seperators.)
 """
 
-from itertools import*;m,p,q,r,s,l,a=dict(enumerate("276951438")),[[],[]],0,print,' │ │ ','─┼─┼─','XO';b=[*map(list,[s,l,s,l,s])]
-def f():r('\n'.join(map(''.join,b)))
-f()
+from itertools import*;m,p,q,r,s,l,a=dict(enumerate("276951438")),[[],[]],0,print,' │ │ ','─┼─┼─','XO';b=[*map(list,[s,l,s,l,s])];f=lambda:r('\n'.join(map(''.join,b)));f()
 while m:
  i=int(input(a[q]+' move: '))-1;p[q]+=[int(m.pop(i))];b[~(i//3*2)][i%3*2]=a[q];f()
  if any(sum(d)==15for d in combinations(p[q],3)):r(a[q]+' wins');break
