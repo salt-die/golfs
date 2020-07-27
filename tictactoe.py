ac46378e9e9bfa2348af35ff558d3cdfc5378f38
salt-die <naclmolecule@gmail.com>
2020-07-27 12:06:09 -0500

    added expanded version

--- tictactoe.py
+++ tictactoe.py
@@ -9,13 +9,6 @@ Note that all the rows, columns, and diagonals sum to 15.
 We translate a player's move into a magic square value and then check if any
 combinations of 3 moves from a player sum to 15. If any do, they win!
 
-p is the list of the two players moves. (p[0] for player X's moves, p[1] for
-player O's)
-
-q is which player's turn it is.
-
-b is our board and f() prints the board
-
 We operate under a few constraints:
     An incorrect move should cause the program to crash or otherwise should
     not be accepted.
@@ -27,6 +20,37 @@ We operate under a few constraints:
     We do, however, need to show the board updated with the players moves
     including the starting and ending positions. (Preferably with horizontal
     and vertical seperators.)
+
+Here's a not-condensed version:
+```
+from itertools import combinations
+
+moves_to_values = dict(enumerate([2, 7, 6, 9, 5, 1, 4, 3, 8]))
+players_values = [[], []]
+current_player = 0
+board = [[' ', '│', ' ', '│', ' '],
+         ['─', '┼', '─', '┼', '─'],
+         [' ', '│', ' ', '│', ' '],
+         ['─', '┼', '─', '┼', '─'],
+         [' ', '│', ' ', '│', ' ']]
+
+def print_board():
+    print('\n'.join(''.join(row) for row in board))
+
+print_board()
+
+while moves_to_values:
+    move = int(input('XO'[current_player] + ' move: ')) - 1
+    players_values[current_player].append(moves_to_values.pop(move))
+    board[~(move // 3 * 2)][move % 3 * 2] = 'XO'[current_player]
+    print_board()
+    if any(sum(d) == 15 for d in combinations(players_values[current_player], 3)):
+        print('XO'[current_player] + ' wins')
+        break
+    current_player ^= 1
+else:
+    print('Draw')
+```
 """
 
 from itertools import*;m,p,q,r,s,l,a=dict(enumerate("276951438")),[[],[]],0,print,' │ │ ','─┼─┼─','XO';b=[*map(list,[s,l,s,l,s])];f=lambda:r('\n'.join(map(''.join,b)));f()
